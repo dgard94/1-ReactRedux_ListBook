5980bc6598b26b85e476c11226b1772eec324db0
Hardiansa <ardititanium@gmail.com>
2018-02-12 00:59:59 +0700

    Initialize and Finish

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good part', pages: 101 },
+    { title: 'Harry Potter', pages: 86 },
+    { title: 'The Dark Tower', pages: 70 },
+    { title: 'Elequent Rubby', pages: 1}
+  ]
+}
